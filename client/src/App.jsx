bc869afb6eaef2f7c240ea8003da1ed1ea6fb555
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-19 04:36:21 -0500

    added book api

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './routes/Home/home'
+import About from "./routes/About/about";
+import Book from "./routes/Book/book";
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/books' element={<Book />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  )
+}
+
+export default App
